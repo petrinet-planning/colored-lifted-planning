f3bbde6e8e6e88aef95804409225b536978584bb
Henrik Ginnerup <hginnerup@hotmail.com>
2023-10-02 18:08:40 +0200

    Update blocksworldnet.py

--- blocksworldnet.py
+++ blocksworldnet.py
@@ -4,8 +4,10 @@ from petrinet.color import *
 from petrinet.literal import EnumerationColorLiteral, DotColorLiteral, ProductColorLiteral
 from petrinet.marking import Marking
 from petrinet.place import Place
+from petrinet.value import ProductColorValue
 from petrinet.variable import *
 from petrinet.transition import *
+from petrinet.weighted_values import WeightedValues
 
 pn = petrinet.PetriNet("Blocksworld_p01")
 
@@ -40,33 +42,33 @@ pred_on = pn.add_place(Place("pred_on", paramColors2))
 # Actions / Transitions
 # Pickup
 act_pickup = pn.add_transition(Transition("act_pickup"))
-pn.add_arc(ArcPlaceToTransition(pred_clear, act_pickup, var_ob))          # delete
-pn.add_arc(ArcPlaceToTransition(pred_on_table, act_pickup, var_ob))       # delete
-pn.add_arc(ArcPlaceToTransition(pred_arm_empty, act_pickup, dotLiteral))  # delete
-pn.add_arc(ArcTransitionToPlace(act_pickup, pred_holding, var_ob))        # add
+pn.add_arc(ArcPlaceToTransition(pred_clear, act_pickup, {var_ob: 1}))          # delete
+pn.add_arc(ArcPlaceToTransition(pred_on_table, act_pickup, {var_ob: 1}))       # delete
+pn.add_arc(ArcPlaceToTransition(pred_arm_empty, act_pickup, {dotLiteral: 1}))  # delete
+pn.add_arc(ArcTransitionToPlace(act_pickup, pred_holding, {var_ob: 1}))        # add
 
 # Putdown
 act_putdown = pn.add_transition(Transition("act_putdown"))
-pn.add_arc(ArcTransitionToPlace(act_putdown, pred_clear, var_ob))          # add
-pn.add_arc(ArcTransitionToPlace(act_putdown, pred_on_table, var_ob))       # add
-pn.add_arc(ArcTransitionToPlace(act_putdown, pred_arm_empty, dotLiteral))  # add
-pn.add_arc(ArcPlaceToTransition(pred_holding, act_putdown, var_ob))        # delete
+pn.add_arc(ArcTransitionToPlace(act_putdown, pred_clear, {var_ob: 1}))          # add
+pn.add_arc(ArcTransitionToPlace(act_putdown, pred_on_table, {var_ob: 1}))       # add
+pn.add_arc(ArcTransitionToPlace(act_putdown, pred_arm_empty, {dotLiteral: 1}))  # add
+pn.add_arc(ArcPlaceToTransition(pred_holding, act_putdown, {var_ob: 1}))        # delete
 
 # Stack
 act_stack = pn.add_transition(Transition("act_stack"))
-pn.add_arc(ArcPlaceToTransition(pred_clear, act_stack, var_ob))          # require pred_clear
-pn.add_arc(ArcTransitionToPlace(act_stack, pred_clear, var_ob))
-pn.add_arc(ArcTransitionToPlace(act_stack, pred_arm_empty, dotLiteral))  # add pred_arm_empty
-pn.add_arc(ArcPlaceToTransition(pred_holding, act_stack, var_ob))        # delete pred_holding
-pn.add_arc(ArcTransitionToPlace(act_stack, pred_on, ProductColorValue(paramColors2, [var_ob, var_underob])))  # add pred_arm_empty
+pn.add_arc(ArcPlaceToTransition(pred_clear, act_stack, {var_ob: 1}))          # require pred_clear
+pn.add_arc(ArcTransitionToPlace(act_stack, pred_clear, {var_ob: 1}))
+pn.add_arc(ArcTransitionToPlace(act_stack, pred_arm_empty, {dotLiteral: 1}))  # add pred_arm_empty
+pn.add_arc(ArcPlaceToTransition(pred_holding, act_stack, {var_ob: 1}))        # delete pred_holding
+pn.add_arc(ArcTransitionToPlace(act_stack, pred_on, {ProductColorValue(paramColors2, [var_ob, var_underob]): 1}))  # add pred_arm_empty
 
 # Unstack
 act_unstack = pn.add_transition(Transition("act_unstack"))
-pn.add_arc(ArcTransitionToPlace(act_unstack, pred_clear, var_ob))          # require pred_clear
-pn.add_arc(ArcPlaceToTransition(pred_clear, act_unstack, var_ob))
-pn.add_arc(ArcPlaceToTransition(pred_arm_empty, act_unstack, dotLiteral))  # delete pred_arm_empty
-pn.add_arc(ArcTransitionToPlace(act_unstack, pred_holding, var_ob))        # add pred_holding
-pn.add_arc(ArcPlaceToTransition(pred_on, act_unstack, ProductColorValue(paramColors2, [var_ob, var_underob])))  # del pred_arm_empty
+pn.add_arc(ArcTransitionToPlace(act_unstack, pred_clear, {var_ob: 1}))          # require pred_clear
+pn.add_arc(ArcPlaceToTransition(pred_clear, act_unstack, {var_ob: 1}))
+pn.add_arc(ArcPlaceToTransition(pred_arm_empty, act_unstack, {dotLiteral: 1}))  # delete pred_arm_empty
+pn.add_arc(ArcTransitionToPlace(act_unstack, pred_holding, {var_ob: 1}))        # add pred_holding
+pn.add_arc(ArcPlaceToTransition(pred_on, act_unstack, {ProductColorValue(paramColors2, [var_ob, var_underob]): 1}))  # del pred_arm_empty
 
 # Markings
 # Initial
